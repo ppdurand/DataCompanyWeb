e0df9647c193b1c0ff991399e274eb197e3ac6f9
Pedro Paulo Durand <pedropaulodurand@gmail.com>
2023-10-24 19:30:55 -0300

    Service adicionada

--- src/main/java/com/example/DataCompanyWeb/controllers/CollaboratorController.java
+++ src/main/java/com/example/DataCompanyWeb/controllers/CollaboratorController.java
@@ -8,6 +8,7 @@ import com.example.DataCompanyWeb.models.Collaborator;
 import com.example.DataCompanyWeb.models.Project;
 import com.example.DataCompanyWeb.repository.CollaboratorRepository;
 import com.example.DataCompanyWeb.repository.ProjectRepository;
+import com.example.DataCompanyWeb.services.CollaboratorService;
 import jakarta.validation.Valid;
 import org.hibernate.query.sql.spi.ParameterOccurrence;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,99 +28,39 @@ import java.util.OptionalInt;
 @RequestMapping("collaborators")
 public class CollaboratorController {
     @Autowired
-    private CollaboratorRepository collaboratorRepository;
-    @Autowired
-    private ProjectRepository projectRepository;
+    private CollaboratorService collaboratorService;
     @GetMapping
     public ModelAndView index(){
-        List<Collaborator> collaborators = this.collaboratorRepository.findAll();
-        ModelAndView mv = new ModelAndView("collaborators/index");
-        mv.addObject("collaborators", collaborators);
-        return mv;
+        return this.collaboratorService.GetAll();
     }
 
     @GetMapping("/new")
     public ModelAndView newCollaborator(NewCollaboratorDTO newCollaborator){
-        ModelAndView mv = new ModelAndView("collaborators/new");
-
-        mv.addObject("collaboratorType", CollaboratorType.values());
-        mv.addObject("collaboratorFunction", CollaboratorFunction.values());
-        mv.addObject("projects", projectRepository.findAll());
-        return mv;
+        return this.collaboratorService.CreateCollaborator();
     }
 
     @PostMapping()
-    public ModelAndView CreateCollaborator(@Valid NewCollaboratorDTO newCollaborator, BindingResult bindingResult){
-        if(bindingResult.hasErrors()){
-            ModelAndView mv = new ModelAndView("collaborators/new");
-            mv.addObject("collaboratorType", CollaboratorType.values());
-            mv.addObject("collaboratorFunction", CollaboratorFunction.values());
-            mv.addObject("projects", projectRepository.findAll());
-            return mv;
-        }
-        Collaborator collaborator = newCollaborator.toCollaborator();
-        collaboratorRepository.save(collaborator);
-        return new ModelAndView("redirect:/collaborators");
+    public ModelAndView CreateCollaborator(NewCollaboratorDTO newCollaborator, BindingResult bindingResult){
+        return this.collaboratorService.PostCollaborator(newCollaborator, bindingResult);
     }
 
     @GetMapping("/{id}")
     public ModelAndView ShowDetails(@PathVariable Long id){
-        Optional<Collaborator> optional = this.collaboratorRepository.findById(id);
-        if(optional.isPresent()){
-            Collaborator collaborator = optional.get();
-            ModelAndView mv = new ModelAndView("collaborators/show");
-            mv.addObject("collaborator", collaborator);
-            return mv;
-        }
-        return new ModelAndView("redirect:/collaborators");
+        return this.collaboratorService.GetById(id);
     }
     @GetMapping("/{id}/edit")
     public ModelAndView EditCollaborator(@PathVariable Long id, EditCollaboratorDTO editCollaborator){
-
-        Optional<Collaborator> optional = this.collaboratorRepository.findById(id);
-        if(optional.isPresent()){
-            Collaborator collaborator = optional.get();
-            editCollaborator.fromCollaborator(collaborator);
-
-            ModelAndView mv = new ModelAndView("collaborators/edit");
-            mv.addObject("editCollaboratorDTO", editCollaborator);
-            mv.addObject("collaboratorType", CollaboratorType.values());
-            mv.addObject("collaboratorFunction", CollaboratorFunction.values());
-            mv.addObject("projects", projectRepository.findAll());
-            return mv;
-        }
-        else {
-            return new ModelAndView("redirect:/collaborators");
-        }
+        return this.collaboratorService.EditCollaborator(id, editCollaborator);
     }
 
     @PostMapping("/{id}")
     public ModelAndView UpdateCollaborator(@PathVariable Long id,@Valid EditCollaboratorDTO editCollaborator,
                                            BindingResult bindingResult){
-        if(bindingResult.hasErrors()){
-            ModelAndView mv = new ModelAndView("collaborators/new");
-
-            mv.addObject("collaboratorType", CollaboratorType.values());
-            mv.addObject("collaboratorFunction", CollaboratorFunction.values());
-            mv.addObject("projects", projectRepository.findAll());
-            return mv;
-        }
-        else{
-            Optional<Collaborator> optional = this.collaboratorRepository.findById(id);
-            if(optional.isPresent()){
-                Collaborator collaborator = editCollaborator.toCollaborator(optional.get());
-                this.collaboratorRepository.save(collaborator);
-                return new ModelAndView("redirect:/collaborators");
-            }
-            else{
-                return new ModelAndView("redirect:/collaborators");
-            }
-        }
+        return this.collaboratorService.UpdateCollaborator(id,editCollaborator, bindingResult);
     }
 
     @GetMapping("{id}/delete")
     public ModelAndView DeleteCollaborator(@PathVariable Long id){
-        this.collaboratorRepository.deleteById(id);
-        return new ModelAndView("redirect:/collaborators");
+        return this.collaboratorService.DeleteCollaborator(id);
     }
 }
---
+++ src/main/java/com/example/DataCompanyWeb/services/CollaboratorService.java
@@ -0,0 +1,133 @@
+package com.example.DataCompanyWeb.services;
+
+import com.example.DataCompanyWeb.DTO.EditCollaboratorDTO;
+import com.example.DataCompanyWeb.DTO.NewCollaboratorDTO;
+import com.example.DataCompanyWeb.enums.CollaboratorFunction;
+import com.example.DataCompanyWeb.enums.CollaboratorType;
+import com.example.DataCompanyWeb.models.Collaborator;
+import com.example.DataCompanyWeb.repository.CollaboratorRepository;
+import com.example.DataCompanyWeb.repository.ProjectRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CollaboratorService {
+    @Autowired
+    private CollaboratorRepository collaboratorRepository;
+    @Autowired
+    private ProjectRepository projectRepository;
+    public ModelAndView GetAll(){
+        List<Collaborator> collaborators = this.collaboratorRepository.findAll();
+        ModelAndView mv = new ModelAndView("/collaborators/index");
+        mv.addObject("collaborators", collaborators);
+        return mv;
+    }
+
+    public ModelAndView GetById(Long id){
+        Optional<Collaborator> optional = this.collaboratorRepository.findById(id);
+        if(optional.isPresent()){
+            Collaborator collaborator = optional.get();
+            ModelAndView mv = new ModelAndView("collaborators/show");
+            mv.addObject("collaborator", collaborator);
+            return mv;
+        }
+        else{
+            ModelAndView mv = new ModelAndView("collaborators/new");
+
+            mv.addObject("collaboratorType", CollaboratorType.values());
+            mv.addObject("collaboratorFunction", CollaboratorFunction.values());
+            mv.addObject("projects", this.projectRepository.findAll());
+
+            return mv;
+        }
+    }
+
+    public ModelAndView CreateCollaborator(){
+        ModelAndView mv = new ModelAndView("collaborators/new");
+
+        mv.addObject("collaboratorType", CollaboratorType.values());
+        mv.addObject("collaboratorFunction", CollaboratorFunction.values());
+        mv.addObject("projects", projectRepository.findAll());
+
+        return mv;
+    }
+    
+
+    public ModelAndView PostCollaborator(NewCollaboratorDTO newCollaboratorDTO, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            ModelAndView mv = new ModelAndView("collaborator/new");
+
+            mv.addObject("collaboratorType", CollaboratorType.values());
+            mv.addObject("collaboratorFunction", CollaboratorFunction.values());
+            mv.addObject("projects", this.projectRepository.findAll());
+
+            return mv;
+        }
+        else{
+            Collaborator collaborator = newCollaboratorDTO.toCollaborator();
+            this.collaboratorRepository.save(collaborator);
+
+            return new ModelAndView("redirect:/collaborators");
+        }
+    }
+
+    public ModelAndView EditCollaborator(Long id, EditCollaboratorDTO editCollaborator){
+        Optional<Collaborator> optional = this.collaboratorRepository.findById(id);
+        if(optional.isPresent()){
+            Collaborator collaborator = optional.get();
+            editCollaborator.fromCollaborator(collaborator);
+
+            ModelAndView mv = new ModelAndView("collaborators/edit");
+            mv.addObject("editCollaboratorDTO", editCollaborator);
+            mv.addObject("collaboratorType", CollaboratorType.values());
+            mv.addObject("collaboratorFunction", CollaboratorFunction.values());
+            mv.addObject("projects", projectRepository.findAll());
+            return mv;
+        }
+        else {
+            return new ModelAndView("redirect:/collaborators");
+        }
+    }
+
+    public ModelAndView UpdateCollaborator(Long id, @Valid EditCollaboratorDTO editCollaborator,
+                                           BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            ModelAndView mv = new ModelAndView("collaborators/new");
+
+            mv.addObject("collaboratorType", CollaboratorType.values());
+            mv.addObject("collaboratorFunction", CollaboratorFunction.values());
+            mv.addObject("projects", projectRepository.findAll());
+            return mv;
+        }
+        else{
+            Optional<Collaborator> optional = this.collaboratorRepository.findById(id);
+            if(optional.isPresent()){
+                Collaborator collaborator = editCollaborator.toCollaborator(optional.get());
+                this.collaboratorRepository.save(collaborator);
+                return new ModelAndView("redirect:/collaborators");
+            }
+            else{
+                return new ModelAndView("redirect:/collaborators");
+            }
+        }
+    }
+
+    public ModelAndView DeleteCollaborator(Long id){
+        Optional<Collaborator> optional = this.collaboratorRepository.findById(id);
+        if(optional.isPresent()){
+            this.collaboratorRepository.deleteById(id);
+            return new ModelAndView("redirect:/collaborators");
+        }
+        else{
+            return new ModelAndView("redirect:/collaborators");
+        }
+    }
+}
