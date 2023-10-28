bcdeff0ccede742989d0b52043c75eb5aa91423e
Pedro Paulo Durand <pedropaulodurand@gmail.com>
2023-10-28 16:17:57 -0300

    home atualização

--- src/main/java/com/example/DataCompanyWeb/controllers/HomeController.java
+++ src/main/java/com/example/DataCompanyWeb/controllers/HomeController.java
@@ -1,12 +1,24 @@
 package com.example.DataCompanyWeb.controllers;
 
+import com.example.DataCompanyWeb.repository.CollaboratorRepository;
+import com.example.DataCompanyWeb.repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
+    @Autowired
+    private ProjectRepository projectRepository;
+    @Autowired
+    CollaboratorRepository collaboratorRepository;
+
     @GetMapping("/")
-    public String home(){
-        return "home";
+    public ModelAndView home(){
+        ModelAndView mv = new ModelAndView("home");
+        mv.addObject("projects", this.projectRepository.findAll().size());
+        mv.addObject("collaborators", this.collaboratorRepository.findAll().size());
+        return mv;
     }
 }
